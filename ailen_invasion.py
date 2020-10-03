9044180a16a4b20979d15f8b0cb2fcd59360f51c
An-LeMi <An-LeMi@noreply.github.com>
2020-10-03 20:00:37 +0700

    Move the ship

--- ailen_invasion.py
+++ ailen_invasion.py
@@ -2,30 +2,27 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
 	# Initialize game and create screen object
 	pygame.init()
-	ai_setting = Settings()
+	ai_settings = Settings()
 	screen = pygame.display.set_mode(
-		(ai_setting.screen_width, ai_setting.screen_height))
+		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
 	#Set background color
-	bg_color = (ai_setting.bg_color)
+	bg_color = (ai_settings.bg_color)
+
+	# Make a ship
+	ship = Ship(ai_settings,screen)
 
 	# Start the main loop for game.
 	while True:
-
-		# What for keyboard and mouse events.
-		for event in pygame.even.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-
-		# Redraw the screen during each pass through the loop. 
-		screen.fill(bg_color)
-
-		#Make the most recently drawn screen visible
-		pygame.display.flip()
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(ai_settings, screen, ship)
 
 run_game()		 
--- ship.py
+++ ship.py
@@ -2,17 +2,37 @@ import pygame
 
 class Ship():
 
-	def __init__(self, screen):
+	def __init__(self, ai_settings, screen):
 		"""dInitialize the ship and set its starting position."""
 		self.screen = screen
+		self.ai_settings = ai_settings
 
 		# Load the ship image and get its rect.
 		self.image = pygame.image.load('image/ship.bmp')
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
 
-		self.rect.centerx = self.screen_rect.centerx 
-		self.rect.bottom = self.screen_rect.bottom
+		self.rect.centerx = self.screen_rect.centerx  # dat o truc x
+		self.rect.bottom = self.screen_rect.bottom    # dat giua 
+
+		self.center = float(self.rect.centerx)   #return value of rect.centerx
+
+		# Movement flag
+		self.moving_right = False 
+		self.moving_left = False
+
+
+	def update(self):
+		"""Update the ship's position based on the movement flag"""
+		# Update the ship's center value, not a rect value
+		if self.moving_right and self.rect.centerx < self.screen_rect.right:
+			self.center += self.ai_settings.ship_speed_factor	
+
+		if self.moving_left and self.rect.centerx > 0:
+			self.center -= self.ai_settings.ship_speed_factor
+
+		# Update rect value base on center
+		self.centerx = self.center	
 
 	def blitme(self):
 		"""Draw the ship at its current location."""
